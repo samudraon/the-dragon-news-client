d92eb60026d947b514dabf8fce7fb3549ab59ae7
samudraon <samudraon@gmailcom>
2023-04-30 00:17:37 +0600

    dynamic ratings added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -6,13 +6,13 @@ import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <Container>
+        <Container className='mt-4'>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-            <div>
+            <div className='d-flex'>
                 <Button variant="danger">Latest</Button>
                 <Marquee className='text-danger'>
                     I can be a React component, multiple React components, or just some text.....
